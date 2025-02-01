f8aa4e3f895dbd092dbc1dc11edf0dac2847ed4e
Zeke <lzdzel@gmail.com>
2025-02-01 15:33:05 -0600

    add README and more tests

---
+++ README.md
@@ -0,0 +1,3 @@
+### Static Site Generator
+
+-   Project from [boot.dev](https://www.boot.dev/)
--- src/test/test_htmlnode.py
+++ src/test/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -10,6 +10,21 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("p", "This is a paragraph", [], {"class": "paragraph"})
         self.assertEqual(repr(node), "HTMLNode(p, This is a paragraph, [], {'class': 'paragraph'})")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph", {"class": "paragraph"})
+        self.assertEqual(node.to_html(), '<p class="paragraph">This is a paragraph</p>')
+
+    def test_no_value(self):
+        node = LeafNode("p", None, {"class": "paragraph"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_no_tag(self):
+        node = LeafNode(None, "This is a paragraph", {"class": "paragraph"})
+        self.assertEqual(node.to_html(), 'This is a paragraph')
 
 if __name__ == '__main__':
     unittest.main()
